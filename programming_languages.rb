e2a0626424ee30756ad77ae1a0d1cb59fcfc876b
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-02-07 01:23:38 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -8,6 +8,10 @@ def reformat_languages(languages)
         languages_new[name] = {:type => nil, :style => []}
         languages_new[name][:type] = value
       end
+    end
+  end
+  languages.each do |styles, names|
+    names.each do |name, type|
       languages_new[name][:style] << styles
     end
   end
