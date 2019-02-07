0ebcca8e2941404c37948f161f1465465cfe749f
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-02-07 01:23:05 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,7 +3,6 @@ require 'pry'
 def reformat_languages(languages)
   languages_new = {}
   languages.each do |styles, names|
-    binding.pry
     names.each do |name, type|
       type.each do |key, value|
         languages_new[name] = {:type => nil, :style => []}
